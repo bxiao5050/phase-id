4b849bef853d1c293d7d4a1f4e03695efef1a7b2
shindousaigo <597917872@qq.com>
2019-01-09 10:31:59 +0800

    ~~~~

--- src/jssdk/DOM/components/hover/index.tsx
+++ src/jssdk/DOM/components/hover/index.tsx
@@ -134,9 +134,9 @@ export default class Hover extends React.Component<{
       style={(() => {
         var combine
         if (this.initialPositionLeft) {
-          combine = { left: '-1.4rem' }
+          combine = { left: '-1.3rem' }
         } else {
-          combine = { right: '-1.4rem' }
+          combine = { right: '-1.3rem' }
         }
         return Object.assign({
           transform: `translate(${this.state.x}px, ${this.state.y}px)`
