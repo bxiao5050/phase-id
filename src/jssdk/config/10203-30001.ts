23fcf601514542055852fc9039b73abf6bf46646
zongjiang.ge <zongjiang.ge@xyroyale.com>
2019-11-14 15:02:31 +0800

    添加泰国火影的网页和ios的配置

---
+++ src/jssdk/config/10203-30001.ts
@@ -0,0 +1,33 @@
+// 火影游戏
+export default {
+  name: 'Ninja Battle',
+  app_key: '249ad8e87d704756be67e53c5ea8e571',
+  fb_app_id: '1304983986332151',
+  language: 'TH',
+  region: 'sg',
+  hoverTop: '.24',
+  hoverFromLeft: true,
+  isPurchasedMark: false,
+  mark_id: {
+    fb: '1304983986332151',
+    markName: {},
+    adjust: {},
+  },
+  download: {
+    android: '',
+    ios: ''
+  },
+  page: {
+    index: {
+      test: 'https://www.narutoh5game.com/test_hyrz/index.html',
+      formal: 'https://www.narutoh5game.com/h5-plays/index.html',
+    },
+    facebook: {
+      messenger: {
+        pc: '',
+        mobile: '',
+      },
+      index: ''
+    }
+  }
+}
