3497750002b47c509bc41ccef0df158bbf543518
zongjiang.ge <zongjiang.ge@xyroyale.com>
2020-01-08 19:06:00 +0800

    修改第四种支付方式

--- src/jssdk/DOM/components/payment/Type4.tsx
+++ src/jssdk/DOM/components/payment/Type4.tsx
@@ -1,51 +1,35 @@
-
-import './Type4.scss'
-import * as React from 'react'
-import Payment from './index'
-import { createLocation } from 'history'
-
+import './Type4.scss';
+import * as React from 'react';
+import Payment from './index';
+import {createLocation} from 'history';
 
 type paymentProps = {
-  Payment: Payment
-}
+  Payment: Payment;
+};
 
-
-export default class Type4 extends React.Component<paymentProps, {}, any>  {
-
-  private index = 4
+export default class Type4 extends React.Component<paymentProps, {}, any> {
+  private index = 4;
 
   render() {
-    return <div className="payment-nav">
-      <h2 className="name">
-        {this.props.Payment.state.paymentDatas[this.index].name}
-      </h2>
-      <ul className="cards">
-        {
-          this.props.Payment.state.paymentDatas[this.index].nodes.map((node, i) => (
-            <li key={i} data-id={i}
+    return (
+      <div className='payment-nav'>
+        <h2 className='name'>{this.props.Payment.state.paymentDatas[this.index].name}</h2>
+        <ul className='cards'>
+          {this.props.Payment.state.paymentDatas[this.index].nodes.map((node, i) => (
+            <li
+              key={i}
+              data-id={i}
               onClick={() => {
-                if (this.props.Payment.state.paymentDatas[this.index].nodes[i].showMethod) {
-                  this.props.Payment.intoPay(this.props.Payment.state.paymentDatas[this.index].nodes[i])
-                } else {
-                  RG.jssdk.Ordering(node).then((OrderRes: OrderRes) => {
-                    if (OrderRes.code === 200) {
-                      this.props.Payment.state.paymentDatas[0] = OrderRes.data
-                      this.props.Payment.props.history.push(createLocation('/type0'))
-                    } else {
-                      console.error(OrderRes.error_msg)
-                    }
-                  })
-                }
-
-              }}>
-              <img src={
-                node.codeImg.replace(/http\:\/{0,2}/, 'https://').replace(/:[0-9]+/, '')
-              } />
+                this.props.Payment.intoPay(
+                  this.props.Payment.state.paymentDatas[this.index].nodes[i]
+                );
+              }}
+            >
+              <img src={node.codeImg.replace(/http\:\/{0,2}/, 'https://').replace(/:[0-9]+/, '')} />
             </li>
-          ))
-        }
-      </ul>
-    </div>
+          ))}
+        </ul>
+      </div>
+    );
   }
-
 }
